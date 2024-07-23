1d98f58f9f774e2cb4982193472e1de26fa3ac9c
Omykronn <58723079+Omykronn@users.noreply.github.com>
2024-07-23 14:40:36 +0200

    Use of toolbox.polynoms.isIn function

--- year2023/day10/part2.py
+++ year2023/day10/part2.py
@@ -1,5 +1,6 @@
 from toolbox.AoCutils import findStart
 from toolbox.Vector import Vector
+from toolbox.polygons import isIn
 
 
 sym2moves = {'|': (Vector((1,0)), Vector((-1,0))),
@@ -9,31 +10,6 @@ sym2moves = {'|': (Vector((1,0)), Vector((-1,0))),
              '7': (Vector((0,-1)), Vector((1,0))),
              'F': (Vector((1,0)), Vector((0,1))),
              '.': ()}
-
-def isIn(point: (int, int), polygon: [(int, int)]) -> bool:
-    degree = len(polygon)
-    even = False
-
-    for i in range(degree):
-        # If point is concerned by an non-horizontal edge
-        if ((polygon[i][0] <= point[0] < polygon[(i + 1) % degree][0] or polygon[(i + 1) % degree][0] < point[0] <= polygon[i][0])
-            and polygon[i][0] != polygon[(i + 1) % degree][0]): 
-            if polygon[i][1] == polygon[(i + 1) % degree][1]:  # vertical edge
-                y_intersec = polygon[i][1]
-            else:
-                y_intersec = (polygon[(i + 1) % degree][1] - polygon[i][1]) * (point[0] - polygon[i][0]) / (polygon[(i + 1) % degree][0] - polygon[i][0]) + polygon[i][1]
-
-            if y_intersec < point[1]:
-                even = not even
-            elif y_intersec == point[1]:  # exactly on the edge
-                return True
-        # If point is on an horizontal edge
-        elif ((polygon[i][1] <= point[1] < polygon[(i + 1) % degree][1] or polygon[(i + 1) % degree][1] < point[1] <= polygon[i][1])
-              and polygon[i][0] == polygon[(i + 1) % degree][0] == point[0]):
-              return True
-
-        
-    return even
 
 
 def possibleMovesFromStart(matrix: [str], i: int, j: int) -> (int, int):
